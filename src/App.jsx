3dc7bcb451c5e3598a64175759f0252911a66e31
Khadija <khadijaaranoor2002@gmail.com>
2024-09-18 09:57:52 +0500

    Fetch functionality created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Icon from "react-icons-kit";
+import { search } from "react-icons-kit/feather/search";
+import { useDispatch } from "react-redux";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState("London");
+  const [unit, setUnit] = useState("Metric");
+
+  const dispatch = useDispatch();
+
+  //Data fetching
+  const fetchData = () => {
+    dispatch(getCityData(city, unit)).then((response) => console.response);
+  };
+
+  const handleCitySearch = (event) => {
+    event.preventDefault();
+  };
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="background">
+      <div className="box">
+        {/* city search form*/}
+        <form
+          autoComplete="off"
+          onSubmit={{ handleCitySearch }}
+          onChange={{ handleCityChange }}
+        >
+          <label>
+            <Icon icon={search} />
+          </label>
+          <input
+            type="text"
+            className="city-input"
+            value={city}
+            required
+            placeholder="Enter city....."
+          />
+          <button type="submit">GO</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
